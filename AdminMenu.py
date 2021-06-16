2d9d6d1e8a1076d8da384dedf7edb8e67e7ac636
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-16 15:14:13 +0100

    Changed file name to match with other file names

---
+++ AdminMenu.py
@@ -0,0 +1,204 @@
+import datetime
+
+def admin_main_menu():
+    print("=================")
+    print("Selection:")
+    print("1. View a customer report")
+    print("2. View per plan report")
+    print("3. View general report")
+    print("4. View all customers")
+    print("5. Exit\n")
+
+    def get_selection():
+        selection = input("Please choose an option (1-5):")
+
+        try:
+            selection = int(selection)
+        except:
+            # Keep asking user a number if the input is invalid
+            get_selection()
+
+        if selection > 5 or selection <= 0:
+           get_selection() 
+        else:
+            return selection
+
+    selection = get_selection()
+
+    if(selection == 1):
+        user_selection_menu()
+    elif(selection == 2):
+        per_plan_report_plan_selection_menu()
+    elif(selection == 3):
+        print("selection 3")
+        general_report_date_range_selection_menu()
+    elif(selection == 4):
+        # get_all_customers()
+        print("selection 4")
+
+
+def user_selection_menu():
+    phonenumber = input("Please enter phone number :")
+    print("run get_cusomter_report_by_phone_number({0})".format(phonenumber))
+
+
+def per_plan_report_plan_selection_menu():
+    print("=================")
+    print("Selection:")
+    print("1. Pay as You Go")
+    print("2. Top-Up-20 Plan")
+    print("3. Premium Plan")
+    print("4. Exit\n")
+
+    def get_plan():
+        selection = input("Please choose a plan(1-3) or Exit(4):")
+
+        try:
+            selection = int(selection)
+        except:
+            # Keep asking user a number if the input is invalid
+            get_plan()
+
+        if selection > 4 or selection <= 0:
+           get_plan() 
+        else:
+            return selection
+
+    plan = get_plan()
+
+    if(plan in range(1, 3)):
+        per_plan_report_date_range_selection_menu()
+    elif(plan == 4):
+        exit()
+
+
+def per_plan_report_date_range_selection_menu():
+    print("=================")
+    print("Selection:")
+    print("1. Last Month")
+    print("2. Custom")
+    print("3. Exit\n")
+
+    def get_selection():
+        selection = input("Please choose a date range(1 or 2) or Exit(3):")
+
+        try:
+            selection = int(selection)
+        except:
+            # Keep asking user a number if the input is invalid
+            get_selection()
+
+        if selection > 3 or selection <= 0:
+            get_selection() 
+        else:
+            return selection
+
+    selection = get_selection()
+
+    if selection == 1:
+        today = datetime.date.today()
+        start = today.replace(day=30)
+        end = today.replace(day=1)
+
+        print("run get_all_topups_by_date_range({0}, {1})".format(start, end))
+
+    elif selection == 2:
+        per_plan_report_custom_date_range_menu()
+
+    elif selection == 3:
+        exit()
+
+
+def per_plan_report_custom_date_range_menu():
+
+    def get_start_date():
+        start = input("Please enter START DATE in dd/mm/yyyy format :")
+
+        try:
+            start = datetime.datetime.strptime(start, "%d/%m/%Y")
+        except:
+            # Keep asking user a date if the input is invalid date format
+            get_start_date()
+
+
+    def get_end_date():
+        end = input("Please enter END DATE in dd/mm/yyyy format :")
+
+        try:
+            end = datetime.datetime.strptime(end, "%d/%m/%Y")
+        except:
+            # Keep asking user a date if the input is invalid date format
+            get_end_date()
+
+    start = get_start_date()
+    end = get_end_date()
+
+    print("run get_all_topups_by_date_range({0}, {1})".format(start, end))
+
+
+def general_report_date_range_selection_menu():
+    print("=================")
+    print("Selection:")
+    print("1. Last Month")
+    print("2. Custom")
+    print("3. Exit\n")
+
+    def get_selection():
+        selection = input("Please choose a date range(1 or 2) or Exit(3):")
+
+        try:
+            selection = int(selection)
+        except:
+            # Keep asking user a number if the input is invalid
+            get_selection()
+
+        if selection > 3 or selection <= 0:
+            get_selection() 
+        else:
+            return selection
+
+    selection = get_selection()
+
+    if selection == 1:
+        today = datetime.date.today()
+        start = today.replace(day=30)
+        end = today.replace(day=1)
+
+        print("run get_all_topups_by_date_range({0}, {1})".format(start, end))
+
+    elif selection == 2:
+        general_report_custom_date_range_menu()
+
+    elif selection == 3:
+        exit()
+
+
+def general_report_custom_date_range_menu():
+
+    def get_start_date():
+        start = input("Please enter START DATE in dd/mm/yyyy format :")
+
+        try:
+            start = datetime.datetime.strptime(start, "%d/%m/%Y")
+        except:
+            # Keep asking user a date if the input is invalid date format
+            get_start_date()
+
+
+    def get_end_date():
+        end = input("Please enter END DATE in dd/mm/yyyy format :")
+
+        try:
+            end = datetime.datetime.strptime(end, "%d/%m/%Y")
+        except:
+            # Keep asking user a date if the input is invalid date format
+            get_end_date()
+ 
+    start = get_start_date()
+    end = get_end_date()
+    # - - - - - - - - -error: start and end returns None- - - - - - - -
+    print("run get_general_report_by_date_range({0}, {1})".format(start, end))
+
+
+# Test run
+admin_main_menu()
