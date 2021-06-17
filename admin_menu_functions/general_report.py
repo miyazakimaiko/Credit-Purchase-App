a91f2a074152d181ac5b362c289351241c6fd346
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-17 15:27:27 +0100

    Changed the file name to match the requirements

--- admin_menu_functions/general_report.py
+++ admin_menu_functions/general_report.py
@@ -1,3 +1,11 @@
+# ===============================================================================
+# REQUIREMENTS
+# 
+# Selection 3. General Plan Report - Two Options: (A)Last Month (B)Date Range 
+# - Display all top-ups - Display "Total:" 
+# ===============================================================================
+
+
 from datetime import datetime
 
 # only accept start and end date format -- dd/mm/yyyy
@@ -7,8 +15,8 @@ def get_all_topups_by_date_range(startDate, endDate):
     # with text file read it and create a list of topups here..
     topups = []
 
-    topup1 = {"phone_number": "123456", "charged": 30, "plan": 1, "date": "17/05/2020"}
-    topup2 = {"phone_number": "343848", "charged": 30, "plan": 2, "date": "17/06/2020"}
+    topup1 = {"phone_number": "123456", "charged": 30, "plan": 1, "date": "18/05/2021"}
+    topup2 = {"phone_number": "343848", "charged": 30, "plan": 2, "date": "17/06/2021"}
     topup3 = {"phone_number": "123456", "charged": 30, "plan": 3, "date": "17/07/2020"}
     topup4 = {"phone_number": "123456", "charged": 30, "plan": 1, "date": "17/04/2020"}
     topup5 = {"phone_number": "123456", "charged": 30, "plan": 1, "date": "17/03/2020"}
@@ -24,23 +32,26 @@ def get_all_topups_by_date_range(startDate, endDate):
     topups.append(topup6)
     topups.append(topup7)
     topups.append(topup8)
+    # demo data end
 
-    # startDate = datetime.strptime(start, "%d/%m/%Y")
-    # endDate = datetime.strptime(end, "%d/%m/%Y")
-    totalTopUp = 0
+    print("Start Date: ", startDate)
+    print("End Date: ", endDate)
 
     # Header of the topup history table
-    print("\nPhone Number".ljust(15) + "Charged".ljust(10) + "Plan".ljust(10) + "Date")
+    print("\n")
+    print("Phone Number".ljust(15) + "Charged".ljust(10) + "Plan".ljust(10) + "Date")
     print("---------------------------------------------------------")
+
+    total_charge = 0
 
     for topup in topups:
         topUpDate = datetime.strptime(topup["date"], "%d/%m/%Y")
 
         if startDate <= topUpDate and endDate >= topUpDate:
-            totalTopUp += topup["topup"]
-            print(f"{topup['phoneNumber'].ljust(15)}{str(topup['charged']).ljust(10)}{str(topup['plan']).ljust(10)}{topup['date']}")
+            total_charge += topup["charged"]
+            print(f"{topup['phone_number'].ljust(15)}{str(topup['charged']).ljust(10)}{str(topup['plan']).ljust(10)}{topup['date']}")
 
-    print("\nTotal Charge: ", totalTopUp, "\n")
+    print("\nTotal Charge: ", total_charge, "\n")
 
 # Test
 
