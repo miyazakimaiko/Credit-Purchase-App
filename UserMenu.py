4c452e2d95414696196b155f6e8ddf882873d88c
Esther2098 <josephesther90@gmail.com>
2021-06-17 15:21:29 +0100

    Update UserMenu.py

--- UserMenu.py
+++ UserMenu.py
@@ -1,10 +1,10 @@
 def mainMenu():
     print("Select an option")
     print("1. View balance") #just here for the purpose of menu
-    print("3. Top Up") 
-    print("4. Change mobile plan")
-    print("5. Reports")
-    print("6. Exit")
+    print("2. Top Up") 
+    print("3. Change mobile plan")
+    print("4. Reports")
+    print("5. Exit")
 
     #loop 
     while True:
@@ -13,33 +13,30 @@ def mainMenu():
             if selection ==1:
                 account_balance()
             elif selection ==2:
-                top_up()
+                toppingUp()
                 break     #breaks the loop to stop continuously looping
             elif selection ==3:
-                change_plan()
+                change_plan() #decided to go with user card details first before choosing amount
                 break
             elif selection ==4:
-                change_plan()
-                break
-            elif selection ==5:
                 reports()
                 break
-            elif selection ==6:
+            elif selection ==5:
                 exit()
                 break
             else:
-                print("Invalid option , please select from 1-6")
+                print("Invalid option , please select from 1-5")
                 #calls on main menu again to get use input
                 mainMenu()
         except ValueError:
-            print("Invalid input , please choose from option 1-6")
+            print("Invalid input , please choose from option 1-5")
 
 
 #functions to be called on through user input     
 def account_balance():
     print("")
 
-def top_up():
+def top_up_amount():
     print("Please select your top up option below")
     print("1. £5")
     print("2. £10")
@@ -74,7 +71,7 @@ def top_up():
                 exit()
             else:
                 print("Invalid option , please select from 1-7")
-                top_up() #calls on top up menu as user input is invalid
+                top_up_amount() #calls on top up menu as user input is invalid
         
         except ValueError:
             print("Invalid input , please choose from option 1-7")
@@ -149,7 +146,56 @@ def reports():
 def exit():
     exit
        
+#top up history 
+def topUpHistory():
+    print("read from text file") #still need to implement
+
+#user entering and validating card details for to up
+def toppingUp():
+    #print("please enter your 16 didgit card number ") this is redundant
+    while True:
+        card_number = input("Please enter your 16 digit card number: ")
+        if not card_number.isdigit(): # check if a string contains a number with .isdigit()
+            print("Enter only numbers\n")
+            continue
+        elif len(card_number) != 16:
+            print ("Enter your 16 digit card number \n")
+            continue
+        else:
+            break
+    #once above is validated the user goes to the next step
+    print("Please enter you expiry date in form MMYY (e.g. 0121) ")
+    while True:
+        expiry_date = input("Enter expiry date here ")
+        if not expiry_date.isdigit(): #validates the user input is in digit form
+            print("Enter numbers only \n") #if not prompts the user to try again
+            continue #continuous loop till user is validated
+        elif len (expiry_date) != 4: #user must input 4 digits
+            print("Please enter a 4 digit expiry number ")
+            continue
+        else:
+            break
+    #once above is validated the user goes to the next step
+    print("Please enter your 3 digit CVC (appears behind your card ")
+    while True:
+        cvc_number = input("Enter CVC here ")
+        if not cvc_number .isdigit():
+            print("Enter numbers only \n")
+            continue
+        elif len (cvc_number) != 3:
+            print("Please enter a 3 digit CVC ")
+            continue
+        else:
+            break
+    #once all card details validated the user can choose top up amount
+    print(top_up_amount()) #calls on fun for user to pick a top up amount
 
 
+        
+    
+        
+        
+   
+       
 #runs main menu 
 mainMenu()
