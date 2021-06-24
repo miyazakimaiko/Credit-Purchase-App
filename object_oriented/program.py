5ef635d6ec0122006115efbe807364963ea12ff3
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-24 13:09:31 +0100

    Created objects

--- object_oriented/program.py
+++ object_oriented/program.py
@@ -1,4 +1,6 @@
 from Classes.user import User
+from Classes.admin import Admin
+from Classes.topup import Topup
 
 
 user_list = []
@@ -6,26 +8,31 @@ user_list = []
 def add_users(): #TODO: include current_balance
 
     user_list.append(User("0871112345", "Emma", "Brown", "emma@brown.ie", "qwerty", 3, 100.00))
-    #user_list.append(User("089940", "firstName":"Emma", "lastName":"Brown", "email":"emma@brown.ie", "password":"qwerty", "plan":"3"})
-    #user_list.append({"phoneNumber": "0861234567", "firstName":"Esther", "lastName":"Smith", "email":"esther@smyth.ie", "password":"123123123", "plan":"1"})
-    #user_list.append({"phoneNumber": "0851231234", "firstName":"Maiko", "lastName":"Watson", "email":"maiko@watson.com", "password":"yuiop12345", "plan":"2"})
-
+    user_list.append(User("0862173794", "Silpa", "Pravin", "silpa@brown.ie", "1234", 1, 200.00))
+    user_list.append(User("0899409520", "Bruna", "Weber", "bruna@brown.ie", "5678", 2, 300.00))
+    user_list.append(User("0834837641", "Maiko", "Miyazaki", "maiko@brown.ie", "1987", 3, 100.00))
     
 
 admin_list = []
 
 
 def add_admin():
-    admin_list.append({"username":"managerTom", "employeeNumber":"1234", "password":"manager1"})
-    admin_list.append({"username":"managerBrian", "employeeNumber":"4321", "password":"manager2"})
+    admin_list.append(Admin("0000", "manager0"))
+    admin_list.append(Admin("1111", "manager1"))
+    admin_list.append(Admin("2222", "manager2"))
+    admin_list.append(Admin("3333", "manager3"))
     
 topup_list = []
 
 def add_topups():
-    topup_list.append()
+    topup_list.append(Topup("0871112345", 30.00, 3, "01/01/2021"))
 
 
 
 add_users()
+add_admin()
+add_topups()
 
 print(user_list)
+print(admin_list)
+print(topup_list)
