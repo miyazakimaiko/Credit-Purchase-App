856b357120589fbe3230885fcd60b0ebdf907e71
Silpa Pravin <silpapravin@rediffmail.com>
2021-06-24 23:20:38 +0100

    viewbalance, changeplan added to usermenu file

--- object_oriented/Classes/usermenu.py
+++ object_oriented/Classes/usermenu.py
@@ -1,6 +1,9 @@
+import os
 class Usermenu:
-   
+    def clear_screen(self):
+        os.system('cls')
     def display_usermenu(self):  # Displaying  user menu
+        self.clear_screen()
         choice="0"
         option=""
         print("")
@@ -14,17 +17,25 @@ class Usermenu:
         choice=input("Please enter your option[1/2/3/4/5] : ")
 
         if (choice=="1"):
-            print("")
+            self.view_balance()
             input("Enter return to continue")
+            self.clear_screen()
+            self.display_usermenu()
         elif(choice=="2"):
-            print("")
+            self.credit_top_up()
             print("Enter return to continue")
+            self.clear_screen()
+            self.display_usermenu()
         elif(choice=="3"):
-            print("")
+            self.change_plan()
             print("Enter return to continue")
+            self.clear_screen()
+            self.display_usermenu()
         elif(choice=="4"):
-            print("")
+            self.view_statement()
             print("Enter return to continue")
+            self.clear_screen()
+            self.display_usermenu()
         elif(choice=="5"):
             exit()
         else:
@@ -32,28 +43,121 @@ class Usermenu:
             option=option.upper()
             if (option=="Y"):
                 print("")
-                #usermenu.display_usermenu()
+                self.display_usermenu()
             else:
                 exit()
 
 
-######## function for displaying balance###
+    ######## function for displaying balance###
+    def view_balance(self):
+        print("")
 
-    #def view_balance(self,user_list):
-    #    phone="0862173794"
+        
+    ##function for credit top up########
+    def credit_top_up(self):
+        choice1=""
+        self.clear_screen()
+        print("    Credit top up")
+        print("    *************")
+        print("1. £5")
+        print("2. £10")
+        print("3. £15")
+        print("4. £20")
+        print("5. £25")
+        print("6. Exit") #will close the app
+        choice1=input("Enter your option :   ")
+        if (choice1 =="1"):
+                print("You have topped up with £5")
+                input("please enter return to continue")
+                self.clear_screen()
+                self.display_usermenu()
+        elif choice1 == "2":
+                    print("You have topped up with £10")
+                    input("please enter return to continue")
+                    self.clear_screen()
+                    self.display_usermenu()
+                   
+        elif choice1 == "3":
+                    print("You have topped up with £15")
+                    input("please enter return to continue")
+                    self.clear_screen()
+                    self.display_usermenu()
+                    
+        elif choice1 =="4":
+                    print("You have topped up with £20")
+                    input("please enter return to continue")
+                    self.clear_screen()
+                    self.display_usermenu()
+                    
+        elif choice1 == "5":
+                    print("You have topped up with £25")
+                    input("please enter return to continue")
+                    self.clear_screen()
+                    self.display_usermenu
+                   
+        elif choice1 == "6":
+                    self.clear_screen()
+                    self.display_usermenu()
+                   
+        else:
+                print("Invalid Entry")
+                self.display_usermenu()
+
+   
+
+
+
+    ####function for changing plan####
+    def change_plan(self):
+        self.clear_screen()
+        choice2=""
+        print("    Change your plan")
+        print("    ****************")
+        print("1. Prepay")
+        print("2. Bill pay")
+        print("3. Sim-only")
+        print("4. Exit")
+        choice2=input("Enter your choice  :  ")
+        if choice2 =="1":
+            print("You have switched over to pre pay")
+            input("Enter return to continue...")
+            self.clear_screen()
+            self.display_usermenu()         
+        elif choice2 =="2":
+            print("You have switched over to bill pay")
+            input("Enter return to continue....")
+            self.clear_screen()
+            self.display_usermenu()
+        elif choice2 =="3":
+            print("You have switched over to a sim-only plan")
+            input("Enter return to continue....")
+            self.clear_screen()
+            self.display_usermenu()
+        elif choice2 =="4":
+            self.clear_screen()
+            self.display_usermenu()
+        else:
+            print("Invalid Entry ...")
+            self.change_plan()
+
+
+    #####function for  view mini statement######
+
+    def view_statement(self):
+        self.clear_screen()
+        print("  Statement")
+        print("  ********* ")
+        input("Enter return to continue.....")
+        self.clear_screen()
+        self.display_usermenu()
 
 
 
 
+       ##################################################
 
-
-
-
-
-
-
-#usermenu=Usermenu()
-#usermenu.display_usermenu()
+usermenu=Usermenu()
+usermenu.display_usermenu()
     
             
 
--- object_oriented/program.py
+++ object_oriented/program.py
@@ -2,9 +2,16 @@ from Classes.user import User
 from Classes.admin import Admin
 from Classes.topup import Topup
 from Classes.usermenu import Usermenu
-
+import os
 
 user_list = []
+admin_list = []
+topup_list = []
+def clearscreen():
+    os.system('cls')
+
+clearscreen()    #  fouction to clear screen
+
 
 def add_users(): #TODO: include current_balance
 
@@ -13,55 +20,19 @@ def add_users(): #TODO: include current_balance
     user_list.append(User("0899409520", "Bruna", "Weber", "bruna@brown.ie", "5678", 2, 300.00))
     user_list.append(User("0834837641", "Maiko", "Miyazaki", "maiko@brown.ie", "1987", 3, 100.00))
     
-
-admin_list = []
-
-
 def add_admin():
     admin_list.append(Admin("0000", "manager0"))
     admin_list.append(Admin("1111", "manager1"))
     admin_list.append(Admin("2222", "manager2"))
     admin_list.append(Admin("3333", "manager3"))
-    
-topup_list = []
 
 def add_topups():
     topup_list.append(Topup("0871112345", 30.00, 3, "01/01/2021"))
 
+add_users()
+add_admin()
+add_topups()
 
-#usermenu=Usermenu()
-#usermenu.display_usermenu()
-
-
-<<<<<<< HEAD
-
-
-#add_users()
-#add_admin()
-#add_topups()
-
-#print(user_list)
-#print(admin_list)
-#print(topup_list)
-=======
-# print(user_list)
-# print(admin_list)
-# print(topup_list)
-
-
-# ====== test for setter -- Working :) ======
-print(user_list[3])
-
-# user_list[3].phone_number = "0000000"
-# user_list[3].first_name = "mmmmmm"
-# user_list[3].last_name = "yyyyy"
-# user_list[3].email_address = "eeeeee"
-# user_list[3].password = "pppppppp"
-# user_list[3].plan = 1
-# user_list[3].add_balance = 10
-# user_list[3].add_topup = Topup("0871112345", 30.00, 3, "01/01/2021")
-
-# Now it changed all the values
-print(user_list[3])
-# ====== test for setter -- Working :) ======
->>>>>>> 655206d218cfa3db2d10ebff6dd38ca9d49923e2
+print(user_list)
+print(admin_list)
+print(topup_list)
