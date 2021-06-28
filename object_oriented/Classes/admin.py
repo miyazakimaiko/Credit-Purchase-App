25ee6be1f1509d5be7cb01812eb6bbc02d553621
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-28 13:15:13 +0100

    Added static variable for employee number

--- object_oriented/Classes/admin.py
+++ object_oriented/Classes/admin.py
@@ -1,8 +1,15 @@
 class Admin:
 
-    def __init__(self, employee_number, password): #SEE IF WE WILL INCLUDE USERNAME??
-        self.employee_number = employee_number
+    # Do we want to generate employee_number automatically
+    # when creating objects...like this?
+    employee_number = 1
+
+    def __init__(self, password): #SEE IF WE WILL INCLUDE USERNAME??
+        self.employee_number = Admin.employee_number
         self.password = password
+
+        # Increment employee number for the next object that will be created
+        Admin.employee_number += 1
 
     def __repr__(self):
         return f"({self.employee_number}, {self.password})"
